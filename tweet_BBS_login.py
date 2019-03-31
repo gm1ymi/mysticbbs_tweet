6f188f90b4c8192c059ca4ce3e450758882177f1
gm1ymi <49123416+gm1ymi@users.noreply.github.com>
2019-03-31 11:10:44 -0400

    Add files via upload

---
+++ tweet_BBS_login.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# tweet.py by Alex Eames http://raspi.tv/?p=5908
+# Modified to tweet BBS connections
+
+import tweepy, sys, datetime
+
+# Consumer keys and access tokens, used for OAuth
+consumer_key = 'Dle8m1UT0PPDiClP3G5EwAfyd'
+consumer_secret = 'Jh1ziP42axm2Mv6tMOk0JkLFaLLImkeCGoAr7MV9moEtutQcK6'
+access_token = '947191999425339394-lFRJF4z42gYMH55wdCrxbqObJeDHkrw'
+access_token_secret = 'vDkyVC79HIzXzK84Arjnmw9zMEHN5PiR9svtc5TpokDDf'
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+time = datetime.datetime.now()
+timenow = '{:%H:%M:%S}'.format(time)
+
+if len(sys.argv) >= 2:
+    tweet_text = sys.argv[1] + " is jivin' with the Cosmik Debris BBS! @ " + str(timenow)
+
+else:
+    tweet_text = "Still messing about with tweepy and twitter API. :)"
+
+if len(tweet_text) <= 280:
+    api.update_status(status=tweet_text)
+else:
+    tweet_text = "UserName too long!"
